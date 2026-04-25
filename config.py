ac5f896257542a4b46102ee0ed3f15115f4eeeb2
officialrohit928-lang <officialrohit928@gmail.com>
2026-04-25 13:28:51 +0530

    Update support channel and chat URLs

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TEAMPURVI/ALPHA_MUSI
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+BCYcD02BEUc3OTc1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Cf8h697DKeI1YTU1")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
